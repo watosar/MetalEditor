455a805da79aec56fd8df57a82e5483151f6d182
watosar/nekojyarasi <38435692+watosar@users.noreply.github.com>
2019-12-30 00:48:31 +0900

    Delete initialize.py

--- mtlviewer/initialize.py
+++
@@ -1,8 +0,0 @@
-from .mtlview import MtlView
-from .shadercontrollerview import ShaderControllerView
-
-def ready(path):
-    mtl_view = MtlView(frame=(0,0,382,382), background_color=(0,.3,0,1))
-    controller_view = ShaderControllerView(shaderview=mtl_view, shaderpath=path)
-    return mtl_view, controller_view
-
