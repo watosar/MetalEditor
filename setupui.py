1ba6f46c47831cae3850b3d9dc02f6cb1e65f6e0
watosar <watosar2113@gmail.com>
2020-01-16 17:20:23 +0000

    kill magic numbers

--- setupui.py
+++ setupui.py
@@ -9,13 +9,15 @@ NSMutableAttributedString = ObjCClass('NSMutableAttributedString')
 UIDeviceRGBColor = ObjCClass('UIDeviceRGBColor')
 rootView = UIApplication.sharedApplication().keyWindow().rootViewController().view()
 
+EDITOR_VIEW_TAG = -1
+BASE_VIEW_TAG = -2
 
 def get_editorView():
-    editorView = rootView.viewWithTag_(-1)
+    editorView = rootView.viewWithTag_(EDITOR_VIEW_TAG)
     if editorView: return editorView
     
     editorView = editor._get_editor_tab().editorView()
-    editorView.setTag_(-1)
+    editorView.setTag_(EDITOR_VIEW_TAG)
     return editorView
 
 
@@ -24,7 +26,7 @@ def get_theme_bgColor():
     return UIColor.colorWithHexString(col)
  
        
-def reapply_editor_theme():
+def reapply_editor_theme(): 
     editorView = get_editorView()
     bgColor = get_theme_bgColor()
     editorView.backgroundColor = bgColor
@@ -36,17 +38,17 @@ def make_baseView():
     width = rootView.frame().size.width
     myView = UIView.alloc().initWithFrame_(CGRect(CGPoint(32, 0), CGSize(width-32, width-32)))
     myView.setAutoresizingMask_(18) #WH
-    myView.setTag_(-2)
+    myView.setTag_(BASE_VIEW_TAG)
     myView.backgroundColor = UIColor.colorWithRed_green_blue_alpha_(0.4, 1, 0.4, 1)
     return myView
     
 
 @on_main_thread
 def setup():
-    baseView = rootView.viewWithTag_(-2)
+    baseView = rootView.viewWithTag_(BASE_VIEW_TAG)
     
     if baseView:
-        tev = rootView.viewWithTag_(-1)
+        tev = rootView.viewWithTag_(EDITOR_VIEW_TAG)
         if not tev:
             sys.stderr.write('cannot start more editor\n')
             return
@@ -70,7 +72,7 @@ def setup():
 #exit()
 if __name__ == '__main__':
     res = setup()
-    ev = rootView.viewWithTag_(-1)
+    ev = rootView.viewWithTag_(EDITOR_VIEW_TAG)
     '''
     import edit_theme
     if res:
